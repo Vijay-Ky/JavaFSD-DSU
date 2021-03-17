d94dbdafff0f3bb0bc175a3209e0750d3d2d867a
Vijay_Ky <vijayky007@gmail.com>
2021-03-17 16:09:23 +0530

    updated

---
+++ abstract and interface/app2/src/C.java
@@ -0,0 +1,11 @@
+interface C
+{
+	static final int i = 10;
+	static final int j = 20;
+	final static int k = 30;
+
+	void test1();
+	public abstract void test2();
+	abstract void test3();
+	abstract void test4();
+}
