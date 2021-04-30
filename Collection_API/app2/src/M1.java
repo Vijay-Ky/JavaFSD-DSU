0ee63a98da0a4db646d90441feaafa1ca1e3505f
Vijay_Ky <vijayky007@gmail.com>
2021-04-30 18:25:39 +0530

    updated

---
+++ Collection_API/app2/src/M1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class M1
+{
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args)
+	{
+		ArrayList list1 = new ArrayList();
+		list1.add(90);
+		list1.add(90);
+		list1.add(90);
+		list1.add(0);
+		list1.add(0);
+		list1.add(0);
+		list1.add("abc");
+		System.out.println(list1);
+	}
+}
