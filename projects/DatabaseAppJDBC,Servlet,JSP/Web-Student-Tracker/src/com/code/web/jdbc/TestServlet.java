e67ae6c0adf623b9ac99b220337afd19579e1c57
Vijay-Ky <vijayky007@gmail.com>
2021-06-11 12:13:05 +0530

    project servlet

---
+++ projects/DatabaseAppJDBC,Servlet,JSP/Web-Student-Tracker/src/com/code/web/jdbc/TestServlet.java
@@ -0,0 +1,71 @@
+package com.code.web.jdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// Define datasource/connection pool for Resource Injection
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Step 1:  Set up the printwriter
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		
+		// Step 2:  Get a connection to the database
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			myConn = dataSource.getConnection();
+			
+			// Step 3:  Create a SQL statements
+			String sql = "select * from student";
+			myStmt = myConn.createStatement();
+			
+			// Step 4:  Execute SQL query
+			myRs = myStmt.executeQuery(sql);
+			
+			// Step 5:  Process the result set
+			while (myRs.next()) {
+				String email = myRs.getString("email");
+				out.println(email);
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
+
+
+
+
+
+
+
